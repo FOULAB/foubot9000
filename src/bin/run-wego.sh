ce012eae441eba613aa41f6e9947491c3188fe88
0x5c <dev@0x5c.io>
2024-07-26 02:47:47 -0400

    météo: add next foulab calendar event
    We now better use the space in the weather panel, while also having a
    nice display for the next event, pulled from the ICS calendar.
    
    Thanks kryma for the original ICS display code!
    
    Closes #3

--- src/bin/run-wego.sh
+++ src/bin/run-wego.sh
@@ -1,4 +1,13 @@
 #! /bin/sh
 
+printf "\033[90mUpdated on "; date '+%a %_d %b %H:%M' ; printf "\033[0m"
+
 wego 0
-printf "\n\033[1;30mLast update: "; date '+%a %_d %b %H:%M:%S' ; printf "\033[0m"
+printf "\n"
+
+# Already used lines
+# wego: 7 + 1
+# date: 1
+# total: 9
+get_events 9
+
